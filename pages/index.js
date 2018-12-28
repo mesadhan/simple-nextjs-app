de31c49da63923dec2b22aa5aeb95eeb4f747e5b
mesadhan <sadhansarker3@gmail.com>
2018-12-28 15:50:40 +0600

    getInitialProps method

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Router from 'next/router'
 import User from '../components/User'
 
+class IndexPage extends Component {
 
-const indexPage = ({ href, name } ) => (
-    <div>
-        <Head>
-            <title>Welcome to next.js</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <link rel="icon" href="/static/loader.png" type="image/gif" sizes="16x16"/>
-        </Head>
-        <h1>Welcome to next.js!</h1>
-        <p style={{ color: 'red' }}>This is paragraph!</p>
+    static getInitialProps(context){
+        console.log('message', context);
 
-        <User name={"Sadhan"} age={23} />
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({'appName': 'Super App'} );
+            },1000)
+        });
 
+        return promise;
+    }
 
-        {' | '}<Link href="/about"><a>About</a></Link>{' | '}
-
-
-        {/*
-            {' | '}<Link href="/auth"><a>Auth</a></Link>{' | '}
-            <Link href={{ pathname: '/about', query: { name: 'Zeit' } }}><a>About</a></Link>{' | '}
-            <Link href="/about" replace>
-                <a>About Replace</a>
-            </Link>{' | '}
-
-            <Link href="/about"><img width={50} src="/static/loader.png" alt="image" /></Link>
-
-            <Link href="/about"><a>About</a></Link> {' | '}
-            <a onClick={() => Router.push('/auth')}>Auth</a> {' | '}
-            <a onClick={() => Router.push('/auth')}>Auth</a> {' | '}
-            <span onClick={handler}>About</span>{' | '}
-        */}
-
-    </div>
-);
-
-
-const handler = () => {
-    Router.push({
-        pathname: '/about',
-        query: { name: 'Zeit' }
-    })
-};
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>Welcome to next.js</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                    <link rel="icon" href="/static/loader.png" type="image/gif" sizes="16x16"/>
+                </Head>
+                <h1>Welcome to next.js!</h1>
+                <h1> The Main Page of {this.props.appName} </h1>
+                <p style={{color: 'red'}}>This is paragraph!</p>
 
 
 
+                <User name={"Sadhan"} age={23}/>
 
-export default indexPage;
+
+                {' | '}<Link href="/about"><a>About</a></Link>{' | '}
+                {' | '}<Link href="/about"><a>Auth</a></Link>{' | '}
+
+            </div>
+        )
+    }
+}
+
+
+export default IndexPage;
